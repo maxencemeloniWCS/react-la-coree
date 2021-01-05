8539764836a871a9a01e3f33539846288c3bc7d5
Maxence <maxence.meloni@wildcodeschool.com>
2021-01-05 12:10:36 +0100

    seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,4 +9,11 @@ module.exports = {
   plugins: [
     `gatsby-env-variables`
   ],
+/*  siteMetadata:{
+    title :"Restaurant La Corée - restaurant barbecue coréen à Lyon",
+    titleTemplate: "%s · Restaurant La Corée - restaurant coréen à Lyon",
+    description : "Saveurs coréennes traditionnel dans une salle conviviale au mobilier brun et aux tables dotées de teppanyaki. Barbecue Coréen",
+    url:"https://www.restaurant-la-coree.fr/",
+    image:""
+  },*/
 }
--- src/html.js
+++ src/html.js
@@ -1,47 +1,58 @@
 import React from "react"
 import PropTypes from "prop-types"
+import {Helmet} from "react-helmet";
 
 export default function HTML(props) {
-  return (
-    <html {...props.htmlAttributes}>
-      <head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        <title>Restaurant La Corée - restaurant coréen à Lyon</title>
-        <meta name="description" content=""/>
+    return (
+        <html {...props.htmlAttributes}>
+        <head>
+            <meta charSet="utf-8"/>
+            <meta httpEquiv="x-ua-compatible" content="ie=edge"/>
+{/*            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1, shrink-to-fit=no"
+            />*/}
+            <link rel="canonical" href="https://www.restaurant-la-coree.fr"/>
+            <meta name="author" content="mmapi.fr" />
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no" />
+            {/*<meta name="viewport" content="width=device-width, initial-scale=1"/>*/}
+            <title>Restaurant La Corée - restaurant barbecue coréen à Lyon</title>
+            <meta name="description"
+                  content="Saveurs coréennes traditionnel dans une salle conviviale au mobilier brun et aux tables dotées de teppanyaki. Barbecue Coréen"/>
+            <meta property="og:url" content="https://www.restaurant-la-coree.fr"/>
+            <meta property="og:type" content="article"/>
+            <meta property="og:title" content="Restaurant La Corée - restaurant barbecue coréen à Lyon"/>
+            <meta property="og:description" content="Saveurs coréennes traditionnel dans une salle conviviale au mobilier brun et aux tables dotées de teppanyaki. Barbecue Coréen"/>
+            <meta property="og:image" content=""/>
 
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,700;1,600&display=swap"
-              rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&display=swap" rel="stylesheet" />
-            <link href="/assets/css/reset.css" rel="stylesheet" />
-            <link href="/assets/fonts/fontawesome/css/all.min.css" rel="stylesheet"/>
+            <link
+                href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,700;1,600&display=swap"
+                rel="stylesheet"/>
+            <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&display=swap" rel="stylesheet"/>
+            <link rel='dns-prefetch' href='//fonts.googleapis.com' />
+            <link rel='dns-prefetch' href='//s.w.org' />
 
             {props.headComponents}
-      </head>
-      <body {...props.bodyAttributes}>
+        </head>
+        <body {...props.bodyAttributes}>
         {props.preBodyComponents}
         <div
-          key={`body`}
-          id="___gatsby"
-          dangerouslySetInnerHTML={{ __html: props.body }}
+            key={`body`}
+            id="___gatsby"
+            dangerouslySetInnerHTML={{__html: props.body}}
         />
         {props.postBodyComponents}
         <script src="https://kit.fontawesome.com/a078a7a723.js" crossOrigin="anonymous"></script>
-
-      </body>
-    </html>
-  )
+        </body>
+        </html>
+    )
 }
 
 HTML.propTypes = {
-  htmlAttributes: PropTypes.object,
-  headComponents: PropTypes.array,
-  bodyAttributes: PropTypes.object,
-  preBodyComponents: PropTypes.array,
-  body: PropTypes.string,
-  postBodyComponents: PropTypes.array,
+    htmlAttributes: PropTypes.object,
+    headComponents: PropTypes.array,
+    bodyAttributes: PropTypes.object,
+    preBodyComponents: PropTypes.array,
+    body: PropTypes.string,
+    postBodyComponents: PropTypes.array,
 }

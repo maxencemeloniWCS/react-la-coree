2943d1a0cd4109440b556b8d8098c60f94740b8b
n1cb0tt <65509016+n1cb0tt@users.noreply.github.com>
2021-01-05 12:52:36 +0100

    add border on gallery images

--- src/Components/Gallery/index.js
+++ src/Components/Gallery/index.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react"
-// import Card from "./Card";
-import "./style.css"
+import React, { useEffect, useState } from 'react';
+import './style.css';
 
 import {
     CarouselProvider,
@@ -8,79 +7,57 @@ import {
     Slide,
     ButtonBack,
     ButtonNext,
-    // ButtonFirst,
-    // ButtonLast,
-    Image,
-    // ImageWithZoom,
-    // Dot,
-} from "pure-react-carousel"
-import "pure-react-carousel/dist/react-carousel.es.css"
+    Image
+} from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 function Gallery() {
-    const [isLoading, setIsLoading] = useState(true)
-    const [cards, setCards] = useState(null)
+    const [isLoading, setIsLoading] = useState(true);
+    const [cards, setCards] = useState(null);
     useEffect(() => {
-        fetch(process.env.GATSBY_API_URL + "/gallery")
+        fetch(process.env.GATSBY_API_URL + '/gallery')
             .then(res => res.json())
             .then(res => {
-                setCards(res.slider)
-                setIsLoading(false)
+                setCards(res.slider);
+                setIsLoading(false);
             })
-            .catch(err => console.log(err))
-    }, [])
+            .catch(err => console.log(err));
+    }, []);
 
     return (
-        <>
-            {/* <section id="gallery">
-                <div className="gallery-arrow" id="arrow-left">
-                    <i className="fa fa-chevron-left" />
-                </div>
-                <div className="gallery-arrow" id="arrow-right">
-                    <i className="fa fa-chevron-right" />
-                </div>
-                <div id="gallery-wrapper">
-                    {isLoading
-                        ? ""
-                        : cards.map(card => <Card card={card} key={card.id} />)}
-                </div>
-            </section> */}
-
+        <section id="gallery">
             {isLoading ? (
-                ""
+                ''
             ) : (
-                    <section id="gallery">
-                        <CarouselProvider
-                            naturalSlideWidth={10}
-                            naturalSlideHeight={10}
-                            totalSlides={cards.length}
-                            visibleSlides={5}
-                        >
-                            <Slider>
-                                <>
-                                    {cards.map((card, i) => (
-                                        <Slide index={i} key={i}>
-                                            <Image
-                                                src={
-                                                    process.env.GATSBY_API_URL +
-                                                    card.image.formats.small.url
-                                                }
-                                            />
-                                            <p>{card.title}</p>
-                                        </Slide>
-                                    ))}
-                                </>
-                            </Slider>
-                            <ButtonBack className="gallery-arrow" id="arrow-left">
-                                <i className="fa fa-chevron-left" />
-                            </ButtonBack>
-                            <ButtonNext className="gallery-arrow" id="arrow-right">
-                                <i className="fa fa-chevron-right" />
-                            </ButtonNext>
-                        </CarouselProvider>
-                    </section>
-                )}
-        </>
-    )
+                <CarouselProvider
+                    naturalSlideWidth={10}
+                    naturalSlideHeight={10}
+                    totalSlides={cards.length}
+                    visibleSlides={4}
+                    isPlaying={true}
+                    infinite={true}
+                    interval={5000}
+                >
+                    <Slider>
+                        <>
+                            {cards.map((card, i) => (
+                                <Slide index={i} key={i}>
+                                    <Image src={process.env.GATSBY_API_URL + card.image.formats.small.url} />
+                                    <p>{card.title}</p>
+                                </Slide>
+                            ))}
+                        </>
+                    </Slider>
+                    <ButtonBack className="gallery-arrow" id="arrow-left">
+                        <i className="fa fa-chevron-left" />
+                    </ButtonBack>
+                    <ButtonNext className="gallery-arrow" id="arrow-right">
+                        <i className="fa fa-chevron-right" />
+                    </ButtonNext>
+                </CarouselProvider>
+            )}
+        </section>
+    );
 }
 
-export default Gallery
+export default Gallery;

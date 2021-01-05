1f42272e1f12f97317f51f46e70c1bc53d39aa74
EthernalSlayer <guillaume.b.a@protonmail.com>
2021-01-05 16:13:28 +0100

    change fetch method to graphql

--- src/Components/Head/index.js
+++ src/Components/Head/index.js
@@ -1,66 +1,67 @@
-import React, { useEffect, useState } from "react"
-import Nav from "./Nav"
-import ContactBar from "./ContactBar"
-import "react-responsive-carousel/lib/styles/carousel.min.css"
-import { Carousel } from "react-responsive-carousel"
+import React from 'react';
+import Nav from './Nav';
+import ContactBar from './ContactBar';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import {Carousel} from 'react-responsive-carousel';
+import {graphql, useStaticQuery} from 'gatsby';
 
 function Header() {
-  const [images, setImages] = useState([])
-  const [title, setTitle] = useState([])
-  const [subtitle, setSubtitle] = useState([])
+    const data = useStaticQuery(graphql`
+        {
+            strapiSlideshow {
+                slider {
+                    image {
+                        url
+                        name
+                    }
+                    subtitle
+                    title
+                    id
+                }
+            }
+        }
+    `);
 
-  useEffect(() => {
-    fetch(process.env.GATSBY_API_URL + "/slideshow")
-      .then(res => res.json())
-      .then(res => res.slider)
-      .then(res => {
-        res.map(item => {
-          setImages(prev => [...prev, item.image])
-          setTitle(prev => [...prev, item.title])
-          setSubtitle(prev => [...prev, item.subtitle])
-        })
-      })
-      .catch(err => console.log(err))
-  }, [])
-
-
-  return (
-    <section id="header">
-      <div id="header-elems">
-        <Nav />
-        <ContactBar />
-      </div>
-      <div id="slideshow">
-        <Carousel
-          autoPlay={true}
-          infiniteLoop={true}
-          interval={5000}
-          showThumbs={false}
-          showStatus={false}
-          showIndicators={false}
-          dynamicHeight={true}
-        >
-          {images.map((image, i) => {
-            return (
-              <div key={i}>
-                <img
-                  src={process.env.GATSBY_API_URL + image.url}
-                  alt={image.name}
-                  style={{ height: "inherit" }}
-                />
-                <div className="slider-title-container">
-                    <p className="slider-title">
-                      {title[i]}
-                    </p>
-                    <p className="slider-subtitle">{subtitle[i]}</p>
-                </div>
-              </div>
-            )
-          })}
-        </Carousel>
-      </div>
-    </section>
-  )
+    return (
+        <section id='header'>
+            <div id='header-elems'>
+                <Nav />
+                <ContactBar />
+            </div>
+            <div id='slideshow'>
+                <Carousel
+                    autoPlay={true}
+                    infiniteLoop={true}
+                    interval={5000}
+                    showThumbs={false}
+                    showStatus={false}
+                    showIndicators={false}
+                    dynamicHeight={true}
+                >
+                    {data.strapiSlideshow.slider.map((item) => {
+                        return (
+                            <div key={item.id}>
+                                <img
+                                    src={
+                                        process.env.GATSBY_API_URL +
+                                        item.image[0].url
+                                    }
+                                    alt={item.image[0].name}
+                                    style={{height: 'inherit'}}
+                                />
+                                <div className='slider-title-container'>
+                                    <p className='slider-title'>{item.title}</p>
+                                    <p className='slider-subtitle'>
+                                        {item.subtitle}
+                                    </p>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </Carousel>
+            </div>
+        </section>
+    );
 }
 
-export default Header
+export default Header;

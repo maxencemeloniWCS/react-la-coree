015eb7c0831a1a4600f457e9a909e87f5869a025
EthernalSlayer <guillaume.b.a@protonmail.com>
2021-01-05 12:25:00 +0100

    make a sticky navbar on scrollTop

--- src/Components/Head/Nav.jsx
+++ src/Components/Head/Nav.jsx
@@ -1,29 +1,93 @@
-import React from 'react';
+import React, {useState, useRef, useEffect, useCallback} from 'react';
 
 function Nav() {
+    const [scrolled, setScrolled] = useState(false);
+
+    let lastScrollTop = useRef(0);
+
+    const handleScroll = useCallback(() => {
+        let st = document.documentElement.scrollTop;
+        const offset = window.scrollY;
+        if (offset > 600) {
+            if (st > lastScrollTop.current) {
+                setScrolled(false);
+            } else {
+                setScrolled(true);
+            }
+            lastScrollTop.current = st;
+        } else {
+            setScrolled(false);
+        }
+    }, []);
 
     function handleClick(e) {
         e.preventDefault();
-        let top = document.getElementById(e.target.getAttribute('href').replace('#', '')).offsetTop;
-        window.scrollTo({top, behavior: 'smooth' });
+        let top = document.getElementById(
+            e.target.getAttribute('href').replace('#', '')
+        ).offsetTop;
+        window.scrollTo({top, behavior: 'smooth'});
     }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', () => handleScroll);
+        };
+    }, [handleScroll]);
+
     return (
-        <header>
-            <div className="wrapper">
-                <h1><a href="/" title=""><span>La</span> Corée</a></h1>
+        <header className={scrolled ? 'fixed' : ''}>
+            <div className='wrapper'>
+                <h1>
+                    <a href='/' title=''>
+                        <span>La</span> Corée
+                    </a>
+                </h1>
                 <nav>
                     <ul>
-                        <li><a onClick={handleClick} href="#header" title="">Accueil</a></li>
-                        <li><a onClick={handleClick} href="#about" title="">À propos</a></li>
-                        <li><a onClick={handleClick} href="#menu" title="">Menu</a></li>
-                        <li><a onClick={handleClick} href="#gallery" title="">Galerie</a></li>
-                        <li><a onClick={handleClick} href="#contact" title="">Nous contacter</a></li>
-                        <li><a href="https://facebook.com/" title="" target="_blank" rel="noreferrer"><i className="fa fa-facebook" aria-label="Facebook" /></a></li>
+                        <li>
+                            <a onClick={handleClick} href='#header' title=''>
+                                Accueil
+                            </a>
+                        </li>
+                        <li>
+                            <a onClick={handleClick} href='#about' title=''>
+                                À propos
+                            </a>
+                        </li>
+                        <li>
+                            <a onClick={handleClick} href='#menu' title=''>
+                                Menu
+                            </a>
+                        </li>
+                        <li>
+                            <a onClick={handleClick} href='#gallery' title=''>
+                                Galerie
+                            </a>
+                        </li>
+                        <li>
+                            <a onClick={handleClick} href='#contact' title=''>
+                                Nous contacter
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href='https://facebook.com/'
+                                title=''
+                                target='_blank'
+                                rel='noreferrer'
+                            >
+                                <i
+                                    className='fa fa-facebook'
+                                    aria-label='Facebook'
+                                />
+                            </a>
+                        </li>
                     </ul>
                 </nav>
             </div>
-        </header>)
+        </header>
+    );
 }
-
 
 export default Nav;

d010ed68db1edcba25538b3f93063e0c1b037d25
Maxence <maxence.meloni@wildcodeschool.com>
2021-01-06 11:36:38 +0100

    misc css fixes

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import FormContainer from "./FormContainer";
 import Map from "./Map";
+import "./Contact.css";
 
 function Contact() {
     return (<section id="contact">

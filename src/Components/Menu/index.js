0e6253da8745979f82e93a23975e3225ffa1237d
EthernalSlayer <guillaume.b.a@protonmail.com>
2021-01-06 17:03:14 +0100

    problem with fragment

--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -1,31 +1,49 @@
-import React from 'react';
-import {graphql, useStaticQuery} from "gatsby";
-import Categories from "./Categories";
-import "./Menu.css";
+import React, {useState} from 'react';
+import {graphql, useStaticQuery} from 'gatsby';
+import Categories from './Categories';
+import './Menu.css';
 
 function Menu() {
-    const data = useStaticQuery(graphql`
-    query Menu {
-      strapiMenu {
-        categories {
-            title
-            description
-            products {
-                title
-                description
-                price
-            }
-        } 
-      }
-    }
-  `);
-    return (<section id="menu">
-        <h3><span>M</span>enu</h3>
-        <div className="wrapper">
-            <Categories categories={data.strapiMenu.categories}/>
-        </div>
-    </section>)
+    const [isOpen, setIsOpen] = useState(false);
 
+    const data = useStaticQuery(graphql`
+        query Menu {
+            strapiMenu {
+                categories {
+                    title
+                    description
+                    products {
+                        title
+                        description
+                        price
+                    }
+                }
+            }
+        }
+    `);
+    return (
+        <section id={isOpen ? 'menuOpen' : 'menuClose'}>
+            {isOpen ? (
+                <>
+                    <h3>
+                        <span>M</span>enu
+                    </h3>
+                    <div className="wrapper">
+                        <Categories categories={data.strapiMenu.categories} />
+                    </div>
+                </>
+            ) : (
+                <>
+                    <h3>
+                        <span>M</span>enu
+                    </h3>
+                    <div className="wrapper">
+                        <Categories categories={data.strapiMenu.categories} />
+                    </div>
+                    <button>Voir</button>
+                </>
+            )}
+        </section>
+    );
 }
 export default Menu;
-

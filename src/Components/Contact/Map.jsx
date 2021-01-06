424b10a8b6779c8ae23b6dd14e18ceac60db1e0b
n1cb0tt <65509016+n1cb0tt@users.noreply.github.com>
2021-01-06 15:24:01 +0100

    add reponsive in contact component

--- src/Components/Contact/Map.jsx
+++ src/Components/Contact/Map.jsx
@@ -8,12 +8,15 @@ function Map() {
                 height="600"
                 title="Google map"
                 frameBorder="0"
-                src={"https://www.google.com/maps/embed/v1/search?key="+process.env.GATSBY_GOOGLE_API_KEY+"&q=record+stores+in+Seattle"} allowFullScreen>
-                        </iframe>
+                src={
+                    'https://www.google.com/maps/embed/v1/search?key=' +
+                    process.env.GATSBY_GOOGLE_API_KEY +
+                    '&q=record+stores+in+Seattle'
+                }
+                allowFullScreen
+            ></iframe>
         </div>
-    )
-
+    );
 }
 
 export default Map;
-

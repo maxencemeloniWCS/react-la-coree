7951dfed35b0ae46e142df8752d7682482c3dfe3
Maxence <maxence.meloni@wildcodeschool.com>
2021-01-06 19:50:05 +0100

    css about

--- src/Components/About/LeftSide.jsx
+++ src/Components/About/LeftSide.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Markdown from "react-markdown";
 
 function LeftSide({title, description}) {
     return (
         <div className="about-side" id="about-left">
             <div>
                 <h3>{title}</h3>
-                <p>{description}</p>
+                <p><Markdown>{description}</Markdown></p>
             </div>
         </div>
     )
